68444cc3eebf3f541f6b29777bc37fe4ef450abe
Mirzaei81 <74964876+Mirzaei81@users.noreply.github.com>
2024-11-24 22:46:18 +0330

    Using raw SQl

--- backend/branch/views.py
+++ backend/branch/views.py
@@ -2,21 +2,24 @@ from rest_framework.viewsets import ReadOnlyModelViewSet,ModelViewSet
 from rest_framework import status
 from rest_framework.response  import Response
 from rest_framework.generics import GenericAPIView,ListAPIView,RetrieveAPIView,UpdateAPIView
-from .models import  Market,User,City,Province,MarketOneTimeSlot
+from .models import  Market,User,City,Province,MarketOneTimeSlot,MarketReserved
 from .serilizers import  MarketSerializer,UserSerializer,CitySerializer,ProvinceSerializer
 from django.db.models import Q
 from datetime import datetime
 from melipayamak import Api
 import jdatetime
-import os
+import json 
+from django.db import connection
 # RestApi = Api(os.getenv("sms_username"),os.getenv("sms_password")).sms()
 class MarketByCityView(GenericAPIView):
 	queryset = Market
 	def get(self,*args,**kwargs):
 		pk = kwargs["pk"]
 		qs = Market.objects.filter(city__pk=int(pk),market_type="mo") # حالت مارکتی که مد نظر برای فیلتر را انتخاب کنید 
-		data = MarketSerializer(qs,many=True)
-		return Response(data.data)
+		cursor = connection.cursor()
+		cursor.execute("SELECT * from (SELECT marketFeature2.*,marketFeature1.* FROM marketFeature2 JOIN marketFeature1 where marketFeature2.info_id = marketFeature1.id) as X join centers_market WHERE X.market_ptr_id = centers_market.id")
+		rows =cursor.fetchall()
+		return Response(json.loads(rows))
 
 class MarketByDateView(GenericAPIView):
 	queryset = Market
@@ -26,6 +29,8 @@ class MarketByDateView(GenericAPIView):
 		endTimeString = self.request.query_params.get("time")
 		endTime = datetime.strptime(endTimeString, "%a, %d %b %Y %H:%M:%S %Z")
 		qs = Market.objects.filter(Q(marketTimeSlots__start_time__gte=startTime)|Q(marketTimeSlots__start_time__lte=endTime),market_type="mo") # حالت مارکتی که مد نظر برای فیلتر را انتخاب کنید 
+		
+
 		data = MarketSerializer(qs,many=True)
 		return Response(data.data)
 		
@@ -36,14 +41,18 @@ class MarketDetailView(RetrieveAPIView,UpdateAPIView):
 		pk = kwargs["pk"]
 		user = self.request.user
 		market = MarketOneTimeSlot.objects.get(id=pk)
-		totalReseveCount =market.totalReseve
-		currentReserve = market.reserveCount
+		cursor = connection.cursor()
+		cursor.execute("SELECT X.h from (SELECT marketFeature2.*,marketFeature1.* FROM marketFeature2 JOIN marketFeature1 where marketFeature2.info_id = marketFeature1.id) as X join centers_market WHERE X.market_ptr_id = centers_market.id")
+		totalReseveCount  =cursor.fetchall()[0]
+		currentReserve = MarketReserved.objects.get(timeSlot__pk=pk)
+		currentReserveCount =currentReserve.count
 		date = market.date
 		formatedDate = jdatetime.date.fromgregorian(year=date.year,month=date.month,day=date.day)
-		if(currentReserve<totalReseveCount):
-			currentReserve+=1
+		if(currentReserveCount<totalReseveCount):
+			currentReserve.count+=1
 			# RestApi.send(user.username,"0911111111",f"رزور شما در تاریخ {market.market.name} در مرکز {formatedDate} با موفقیت انجام شد")
 			market.save()
+			currentReserve.save()
 			return Response({"data":"با موفقیت انجام شد"},status=status.HTTP_200_OK)
 		else:
 			return Response({"data":"تعداد رزرو ها پر شده است"},status=status.HTTP_400_BAD_REQUEST)
